cf08f3477c2f9f163aebab277b059ab48ee2de12
Arun <arunmtsp@outlook.com>
2026-07-06 09:44:10 +0530

    Addming Tags

--- main.tf
+++ main.tf
@@ -16,4 +16,7 @@ provider "aws" {
 resource "aws_instance" "Raminfra" {
 	ami = "ami-0b6d9d3d33ba97d99" 
 	instance_type = "t3.micro"
+        tags = { 
+                "Name" = "Arun-GIT"
+        }
 }
